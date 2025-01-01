83f61ac07a900244fc24b93ee01f182e09132162
jeermuce <ernesto_mc.dev@proton.me>
2025-01-01 03:41:10 -0600

    Doc

--- src/modules/arguments/args.rs
+++ src/modules/arguments/args.rs
@@ -5,6 +5,7 @@ use super::parse_path::parse_path;
 #[derive(Parser, Debug)]
 #[command(term_width = 0)]
 pub struct Args {
+    /// The path to the directory to process
     #[arg(short = 's', long = "source", value_name = "DIR", value_parser = parse_path, default_value = "./src")]
     pub source: PathBuf,
 }
